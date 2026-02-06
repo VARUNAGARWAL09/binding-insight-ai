bbb4ffeabd636a4baa26a61c9dd68f0f635fb7e3
Varun Agarwal <varunagarwal0964@gmail.com>
2026-02-06 16:02:26 +0530

    Fix: Add basename to Router for GitHub Pages subdirectory support

--- src/App.tsx
+++ src/App.tsx
@@ -27,7 +27,7 @@ const App = () => (
         <AssistantProvider>
           <Toaster />
           <Sonner />
-          <BrowserRouter>
+          <BrowserRouter basename={import.meta.env.BASE_URL}>
             <Routes>
               <Route path="/" element={<Index />} />
               <Route path="/prediction" element={<Prediction />} />
